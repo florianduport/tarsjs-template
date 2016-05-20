0071316d1b9efcf42ec01feb0103fa8142a856b4
florianduport@gmail.com <florianduport@gmail.com>
2016-05-20 19:03:45 +0200

    start scripts aside

---
+++ api/scripts/start.sh
@@ -0,0 +1,3 @@
+rm -f /data/src/node_modules;
+ln -s /install/node_modules /data/src;
+sails lift
---
+++ front/scripts/start.sh
@@ -0,0 +1,4 @@
+rm -f /data/src/node_modules;
+ln -s /install/node_modules /data/src;
+nginx;
+cd /data/src && grunt dev
